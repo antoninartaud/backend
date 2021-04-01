9e11f07bfa1acce6c3c50b703f3a1068a07a5180
antonin <antoninartaud@hotmail.fr>
2021-04-01 17:34:50 +0200

    config bonus1

---
+++ api-countries/indexServer.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get();
+
+let port = 8000;
+app.listen(port, function () {
+  console.log(`Server on port: ${port} is pogotting`);
+});
