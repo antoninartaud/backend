1b16f4f1d333895e867aaf2497014a4ecd4d4f93
antonin <antoninartaud@hotmail.fr>
2021-06-07 14:30:04 +0200

    Garage ex  Etape 1 Connexion

--- database/garage/db.js
+++ database/garage/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect("mongodb://localhost:27017/students", (err) => {
-//     if (err) {
-//         console.error(err)
-//     } else {
-//         console.log("I'm connected to the database");
-//     }
-// })
+mongoose.connect('mongodb://localhost/garage', (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("I'm connected to the database");
+  }
+});
