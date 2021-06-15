fbbd5a1259e29a5b58aeded56c863ba2aa4eff3e
antonin <antoninartaud@hotmail.fr>
2021-06-15 17:29:02 +0200

    feat: route get users à finir

--- chapitre_04/validation/server.js
+++ chapitre_04/validation/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const userModel = require('./model/user');
 const expressValidator = require('express-validator');
 
 mongoose.connect('mongodb://localhost:27017/validation', (err) => {
@@ -17,9 +18,26 @@ const port = 8000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('On the route / my friend !');
+app.get('/users', async (req, res) => {
+  // res.send('On the route / my friend !');
+  try {
+    const users = await User.find();
+
+    res.json(users);
+  } catch {
+    console.log(error);
+    res.json({
+      errorMessage: 'Sorry Dude error find users',
+    });
+  }
 });
+
+app.get('*', (req, res) => {
+  res.status(404).json({
+    errorMessage: 'This is a dead end my friend !',
+  });
+});
+
 app.listen(port, () => {
   console.log(`Major Tom on port ${port}`);
 });
